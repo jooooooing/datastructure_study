9a0367845d30c209acd6681596f23a56956db7da
jooooooing <leegun1018@naver.com>
2022-08-03 18:54:24 +0900

    Initial commit

---
+++ src/list/arrayList/implementation/Main.java
@@ -0,0 +1,18 @@
+package list.arrayList.implementation;
+
+public class Main {
+
+	public static void main(String[] args) throws java.lang.Exception {
+
+		ArrayList numbers = new ArrayList();
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+		numbers.addLast(40);
+
+		numbers.add(1, 15);
+		numbers.addFirst(5);
+		System.out.println(numbers);
+	}
+
+}
